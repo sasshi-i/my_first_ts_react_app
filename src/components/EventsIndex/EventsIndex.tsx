e6ec6278180622ca5370aab3d5d9bcd4fd44c812
聡志伊藤 <i.satoshi.0915@gmail.com>
2020-05-27 23:33:53 +0900

    implements eventsIndex

--- src/components/EventsIndex/EventsIndex.tsx
+++ src/components/EventsIndex/EventsIndex.tsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { readEvents } from '../../actions'
+import _ from 'lodash'
 
 interface Props {
   readEvents: () => void
+  events: {key: IEvent}
+}
+
+interface IEvent {
+  id: number
+  title: string
+  body: string
 }
 
 interface State {
-  events: {key: string}
+  events: {key: IEvent}
 }
 
 // ToDo
@@ -18,9 +26,31 @@ class EventsIndex extends Component<Props> {
     console.log(this.state)
     this.props.readEvents()
   }
+
+  renderEvents() {
+    return _.map(this.props.events, (event) => (
+      <tr key={event.id}>
+        <td>{event.id}</td>
+        <td>{event.title}</td>
+        <td>{event.body}</td>
+      </tr>
+    ))
+  }
+
   render(){
     return(
-      <div>hogehoge</div>
+      <table>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>Title</td>
+            <td>Message</td>
+          </tr>
+        </thead>
+        <tbody>
+          {this.renderEvents()}
+        </tbody>
+      </table>
     )
   }
 }
